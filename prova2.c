2be5e5219f455ba0864b35b274579636eb66494d
William000427 <wcarlessotelles@gmail.com>
2024-10-14 22:11:19 -0300

    Add files via upload

---
+++ prova2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+	int M,N;
+	
+	while(1) {
+	printf(" M,N ");
+	scanf(%d,%d,%d, &M, &N) ! = 2) {
+		printf("Entrada negada.\n");
+		while(getchar() ! = \n);
+		
+	}
+	
+	if(M = 0, N = 0) {
+			break
+	}
+	
+	int menor = (M < N) ? M:N;
+	int maior = (M > N) ? M:N;
+	int soma = 0;
+	
+	printf("Sequencia:");
+	for(int i = maior, i++) {
+		printf("%d",i);
+		soma += i;
+		
+	}
+	
+	printf("\nSoma = %d\n",Soma);
+	
+	}
+	
+	
+	return(0); 
+	
+}
